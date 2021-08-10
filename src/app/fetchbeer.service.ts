34c85f99f8dca2bd8549eeb4c6e4e0ef8319b445
Swagato Bhaskar <bhaskarswagato@gmail.com>
2021-08-11 00:56:21 +0530

    fetchbeer service additions

--- src/app/fetchbeer.service.ts
+++ src/app/fetchbeer.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FetchbeerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  const options= {
+    headers?: HttpHeaders | {[header: string]: string | string[]};
+    observe?: 'body' | 'events' | 'response';
+    params?: HttpParams|{[param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>};
+    reportProgress?: boolean;
+    responseType?: 'arraybuffer'|'blob'|'json'|'text';
+    withCredentials?: boolean;
+  };
+
+  getBeerList(){
+    return this.http.get()
+  }
 }
