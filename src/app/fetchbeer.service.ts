a3b175d28416432482cb03978e25294bf8b1c500
Swagato Bhaskar <bhaskarswagato@gmail.com>
2021-08-11 23:15:23 +0530

    beer list HttpClient get and Beer interface

--- src/app/fetchbeer.service.ts
+++ src/app/fetchbeer.service.ts
@@ -3,23 +3,99 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
+export interface Beer{
+  id: number;
+  name: string;
+  tagline: string;
+  first_brewed: string;
+  description: string;
+  image_url: string;
+  abv: number;
+  ibu: number;
+  target_fg: number;
+  target_og: number;
+  ebc: number;
+  srm: number;
+  ph: number;
+  attenuation_level: number;
+  volume: {
+    value: number,
+    unit: string
+  };
+  boil_volume: {
+    value: number,
+    unit: string
+  };
+  method: {
+    mash_temp: [{
+      temp: {
+        value: number,
+        unit: string
+      },
+      duration: number
+    }];
+    fermentation: {
+      temp: {
+        value: number,
+        unit: string
+      }
+    };
+    twist: any  // takes null
+  };
+  ingredients: {
+    malt: [{
+      name: string,
+      amount: {
+        value: number,
+        unit: string
+      }
+    }];
+    hops: [{
+      name: string,
+      amount: {
+        value: number,
+        unit: string
+      },
+      add: string,
+      attribute: string
+    }];
+    yeast: string;
+  };
+  food_pairing: string[];
+  brewers_tips: string;
+  contributed_by: string;
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class FetchbeerService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  const options= {
-    headers?: HttpHeaders | {[header: string]: string | string[]};
-    observe?: 'body' | 'events' | 'response';
-    params?: HttpParams|{[param: string]: string | number | boolean | ReadonlyArray<string | number | boolean>};
-    reportProgress?: boolean;
-    responseType?: 'arraybuffer'|'blob'|'json'|'text';
-    withCredentials?: boolean;
-  };
+  httpHeader = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
 
-  getBeerList(){
-    return this.http.get()
+  punkUrl = 'https://api.punkapi.com/v2/beers';
+
+  getBeerList(): Observable<Beer>{
+    return this.httpClient.get<Beer>(this.punkUrl, {responseType: 'json'})
+      .pipe(
+        retry(2), catchError(this.processError)
+      )
+  }
+
+  processError(err){
+    let message = '';
+    if(err.error instanceof ErrorEvent) {
+      message = err.error.message;
+    } else {
+      message = `Error Code: ${err.status}\nMessage: ${err.message}`;
+    }
+    console.log(message);
+    return throwError(message);
   }
 }
