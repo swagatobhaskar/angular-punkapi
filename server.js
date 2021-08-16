30f3f69b6b710b220714629a649fdea1c48bf396
Swagato Bhaskar <bhaskarswagato@gmail.com>
2021-08-17 00:41:21 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+function requireHTTPS(req, res, next) {
+    // the 'x-forwarded-proto' check is for heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https'){
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+const express = require('express');
+const app = express();
+app.use(requireHTTPS);
+
+// serve our static files
+app.use(express.static('./dist/beer-store.json'));
+
+// Wait for a request to any path and redirect all of the requests to 
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/beer-store.json/'});
+});
+
+// Listen for requests at the PORT specified by env variables or the default Heroku port, which is 8080.
+app.listen(process.env.PORT || 8080);
