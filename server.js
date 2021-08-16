c95e830a5b93a633e2406cc4bbebb2053ece82a5
Swagato Bhaskar <bhaskarswagato@gmail.com>
2021-08-17 01:17:55 +0530

    added nodemon

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-function requireHTTPS(req, res, next) {
-    // the 'x-forwarded-proto' check is for heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https'){
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+// function requireHTTPS(req, res, next) {
+//     // the 'x-forwarded-proto' check is for heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https'){
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
 
 const express = require('express');
 const app = express();
-app.use(requireHTTPS);
+const path = require("path");
+// app.use(requireHTTPS);
 
 // serve our static files
-app.use(express.static('./dist/beer-store.json'));
+app.use(express.static('./dist/beer-store'));
 
 // Wait for a request to any path and redirect all of the requests to 
 app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/beer-store.json/'});
+    res.sendFile('index.html', {root: 'dist/beer-store/'});
 });
 
 // Listen for requests at the PORT specified by env variables or the default Heroku port, which is 8080.
